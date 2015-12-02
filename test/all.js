04dcfabfef0876c5ed0c2a1b3b67313471b374d1
Muhamed Krlic <1l0oky1@gmail.com>
2015-12-02 19:39:32 +0100

    Testing Mocha..

---
+++ test/all.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect,
+  fs = require('fs'),
+  rimraf = require('rimraf');
+
+describe('Just some tests', function(){
+  it('should pass', function() {
+    done();
+  });
+});
